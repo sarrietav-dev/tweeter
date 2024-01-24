645403f8144ff1098c774267b24eddf22b7df3ef
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-23 21:33:23 -0500

    Add Button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface ButtonProps {
+  label: string;
+  type: "submit" | "button";
+  icon?: IconProp;
+  onClick?: () => void;
+}
+
+export default function Button({ label, type, icon, onClick }: ButtonProps) {
+  return (
+    <button type={type} className='bg-blue-500 w-full px-5 py-2 text-white'>
+      {icon && <FontAwesomeIcon icon={icon} size='xl' height={100} className="inset-y-3 left-3 text-gray-500" />}
+      {label}
+    </button>
+  )
+}
