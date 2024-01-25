549f3028b890579c0c8cc030b9a9267f44b3a0db
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-25 15:17:26 -0500

    Update IconButton styles

--- components/IconButton/index.tsx
+++ components/IconButton/index.tsx
@@ -10,8 +10,8 @@ interface IconButtonProps {
 
 export default function IconButton({ type, icon, onClick }: IconButtonProps) {
   return (
-    <button type={type} onClick={onClick} className='size-12 rounded-full border-2 border-zinc-500'>
-      <FontAwesomeIcon icon={icon} size='xl' height={100} className="inset-y-3 left-3 text-gray-500" />
+    <button type={type} onClick={onClick} className='group size-12 rounded-full border-2 border-zinc-500 transition hover:bg-zinc-500'>
+      <FontAwesomeIcon icon={icon} size='xl' height={100} className="inset-y-3 left-3 text-gray-500 transition group-hover:text-white" />
     </button>
   )
 }
