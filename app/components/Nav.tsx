28fad99b0f569021fcae428f3404ba4407718323
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-02-01 22:48:26 -0500

    Update Nav component to display different logo sizes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Nav() {
   return (
     <nav className="flex h-16 px-5">
-      <Image src="/tweeter.svg" alt="Tweeter" width={150} height={30} />
+      <Image src="/tweeter.svg" className="hidden md:block" alt="Tweeter" width={150} height={30} />
+      <Image src="/tweeter-small.svg" className="block md:hidden" alt="Tweeter" width={50} height={30} />
     </nav>
   )
 }
