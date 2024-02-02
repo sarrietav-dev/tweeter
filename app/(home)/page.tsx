f0a2da9fd3b3b90667fb5aa341903c676c69a65b
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-02-02 17:12:55 -0500

    Add tweet form to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,53 @@
+"use client";
+
+import Avatar from "@/components/Avatar";
+import { faEarth, faImage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
+
 export default function Home() {
+  const textbox = useRef<HTMLTextAreaElement>(null);
+
+  function handleTextareaHeight(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    const element = e.target as HTMLTextAreaElement;
+    element.style.height = "auto";
+    element.style.height = `${element.scrollHeight}px`;
+  }
+
   return (
-    <h1>Home</h1>
+    <main className="p-4">
+      <div className="rounded-md bg-white p-3">
+        <p className="font-poppins text-xs font-semibold text-gray-2">Tweet something</p>
+        <hr className="my-2" />
+        <form>
+          <div className="flex items-start">
+            <Avatar size="lg" />
+            <textarea
+              className="max-h-60 w-full resize-none border-none placeholder:text-gray-4 focus:outline-none focus:ring-0"
+              onInput={handleTextareaHeight}
+              ref={textbox}
+              maxLength={280}
+              placeholder="What's happening?"
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="space-x-3">
+              <button type="button" className="rounded p-1 text-blue-500 transition active:bg-gray-100">
+                <FontAwesomeIcon icon={faImage} size="lg" />
+              </button>
+              <button type="button" className="rounded p-1 text-blue-500 transition active:bg-gray-100">
+                <FontAwesomeIcon icon={faEarth} size="lg" />
+                <span className="ml-2 text-sm">
+                  Everyone can reply
+                </span>
+              </button>
+            </div>
+            <button type="submit" className="rounded bg-blue-500 px-7 py-2.5 text-xs text-white">
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
